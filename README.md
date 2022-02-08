3e910127ab7727981e646eb89ed54cc1103c078f
https://uconn.bootcampcontent.com/theknightmoores-xJPRH <theknightmoores@gmail.com>
2022-02-08 16:38:56 -0500

    added some variables and began the function for passwordGenerate

--- README.md
+++ README.md
@@ -1,10 +1,45 @@
 # Password Generator Starter Code
+Code for html and css were given. JavaScript must be written by students.
+## User Story
 
-## Pseudocoding notes
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
 
-Upon clicking on the provided "generate password" button I need a form to show up below it. Users will be able to click their password options using toggle switches for each of the listed prompts. They will also be told to enter a number between 8 and 128 for the length of their password. Upon setting their password criteria they will click on a second button that will generate their new ransomized passwor and display it in the original password text area that was provided in the code given to the class. 
+### Acceptance Criteria
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+<!-- A series of window.prompts seems the easiest route though its ugly. I had thought about trying to go the route of toggle switches in a pop up form that would be displayed after pushing the generate button. But this seemed a little advanced for me at the moment and time constraints required I do otherwise. But I really don't like the way this is going to look.-->
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+<!-- -->
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+<!-- I think this will require an if else statement. -->
 
-I need an event listener for second button.
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+<!-- I have four arrays set up for these that are populated with all of the possible characters. To be safe, I commented out the special characters. Knowing that some would break the code but not being sure about all of them, I opted to do this for all of them. -->
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+
+<!-- I believe this will require a series of if/else statements as well.-->
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+### Pseudocoding notes
+
+Upon clicking on the provided "generate password" button I need a form to show up below it. 
+<!-- To start with, in the html file, I added a <main> section to wrap around the given <div class="wrapper"> and a new <form> element. -->
+Users will be able to click their password options using toggle switches for each of the listed prompts. 
+
+They will also be told to enter a number between 8 and 128 for the length of their password. 
+
+Upon setting their password criteria they will click on a second button that will generate their new ransomized passwor and display it in the original password text area that was provided in the code given to the class. 
+
 I need a toggle switch for each of the following prompts, except for #1 which will need a text input area.
 
 Toggle Switch Resources:
@@ -19,6 +54,7 @@ Prompt 1=  choose a length of at least 8 characters and no more than 128 charact
 	Failsafe 1 = if number is <8 => throw error.
 	Failsafe 2 = if number is >128 => throw error.
 	Probably written as a for/ or while loop?
+<!-- This prompt will be var characterLength = function() { var characterLength = window.prompt ("Pick a password length between 8 and 128 characters."); if (characterLength <8 || characterLength >128) {window.alert("Password length must be between 8 and 128 characters long")} )}  -->
 
 Prompt 2 = whether or not to include lowercase
     Validation 2 = return is true or false
--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,55 @@
 // Assignment code here
-// List of arrays for optional password characters; an auto-generator (like I saw on Traversy Media's youtube) for these may be more ellegent but this was quicker for me for my first JavaScript project.
-let lowerCase=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-let upperCase=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-let specialChar=["\!", "\#", "\$", "\%", "\&", "\'", "\(", "\)", "\*", "\+", "\,", "\-", "\.", "\/","\:", "\;", "\<", "\=", "\>","\?", "\@","\[", "\\","\]", "\^", "\_", "\`","\{", "\|","\}", "\~"]
-let numbers=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-let passwordLength=""
-
-//
-function passwordLength {
- 
-
-} 
+// List of arrays for optional password characters.
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var specialChar = ["\!", "\#", "\$", "\%", "\&", "\'", "\(", "\)", "\*", "\+", "\,", "\-", "\.", "\/","\:", "\;", "\<", "\=", "\>","\?", "\@","\[", "\\","\]", "\^", "\_", "\`","\{", "\|","\}", "\~"]
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 
+function generatePassword(/*isLowerCase, isUpperCase, isNumbers, isSpecialChar*/) {
+  // create a statement that will generate a random string using the variables created above your generatePassword function. Hint I think you can use Math.random() here. Start by generating passwords with all variables and then slowly omit variables one at a time. 
+var total = lowerCase + "," + upperCase + "," + numbers + "," + specialChar;
+  console.log(total);
+  //adjust statement to request a length from the user that is between 8 and 128 characters long.
 
+  //adjust the statement to omit numbers from the generated string.
+
+  //adjust the statement to omit Special Characters from the string. And so forth with Upper Case and Lower Case letters.
+  
+  /*var passwordLength = window.prompt("How long should your password be? Passwords can be any length between 8 and 128 characters.");
+  if(passwordLength.length <8 || passwordLength.length >128) {
+    window.alert("please enter a password length between 8 and 128 characters.");
+    console.log(passwordLength.length);
+  }*/
+  
+  var isLowerCase=window.confirm("do you want your password to utilize lower case letters? Click OK for yes or Cancel for no.");
+  if (isLowerCase === false) {
+    var total = upperCase + "," + numbers + "," + specialChar;
+  console.log(total);
+  }
+
+  // var isUpperCase=window.confirm("do you want your password to utilize upper case letters? Click OK for yes or Cancel for no.");
+  // if (isUpperCase === false) {
+  //   var total = lowerCase + "," + numbers + "," + specialChar;
+  // console.log(total);
+  // } else if (isLowerCase === false && isUpperCase === false) {
+  //   var total = numbers + "," + specialChar;
+  // }
+
+  // var isSpecialChar=window.confirm("do you want your password to utilize special characters? Click OK for yes or Cancel for no.");
+  // if (isSpecialChar === false) {
+  //   var total = lowerCase + "," + uppperCase + "," + numbers;
+  // console.log(total);
+  // } else if (isLowerCase === false && isUpperCase === false && specialChar === false) {
+  //   var total = numbers;
+  // }
+  // var isNumbers=window.confirm("do you want your password to utilize numbers? Click OK for yes or Cancel for no.");
+  // if (isNumbers === false) {
+  //   var total = lowerCase + "," + uppperCase + "," + specialChar;
+  // console.log(total);
+  // } else if (isLowerCase === false && isUpperCase === false && specialChar === false && numbers === false) {
+  //   windows.prompt("you must select at least one character type")
+  // }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -24,8 +60,10 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  // console.log(password.length);
+  
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+}
